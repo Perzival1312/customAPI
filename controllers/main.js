875e66c63c6ba43fb09808f9b2090f64fe245bab
Perzival1312 <jj.mongoose11@hotmail.com>
2019-03-04 19:10:47 -0800

    added admin routes

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
+const Messages = {
+    "success": "It was a success!",
+    "unauthorirzed": "You are not Authorized",
+    "unauthenticated": "You are not Authenticated",
+}
 
 router.get('/api/v1', (req, res) => {
     data = {
@@ -13,5 +19,28 @@ router.get('/api/v1', (req, res) => {
        }
     res.json(data)
 })
+
+// Admin only
+router.post('/api/v1/sign-up', (req, res) => {
+    user = {
+        "name": req.params.name,
+        "type": req.params.type,
+        "skills": req.params.skills,
+        "email": req.params.email,
+        "bio": req.params.bio,
+    }
+    User.create(user)
+    res.json(Messages.success)
+})
+
+// Admin only
+router.get('/index', (req, res) => {
+    arr = {}
+    User.find({}).then((users) => {
+        res.json(users)
+    })
+})
+
+
 
 module.exports = router 
