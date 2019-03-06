130d0976f2b8628c8df765414afe4aa4bf6ea7d4
Jake Shams <jshams62@gmail.com>
2019-03-05 18:46:45 -0800

    typos

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -12,17 +12,17 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/api/v1/users')
-    .get(/* secure this route*/, allUsers)
+    .get(/* secure this route*/ allUsers)
     .post(signupUser)
 
 
 router.post('/api/v1/users/login', loginUser);
-router.get('/api/v1/users/logout', /* secure this route*/,logoutUser);
+router.get('/api/v1/users/logout' /* secure this route*/,logoutUser);
 
 router.route('/api/v1/users/:id')
-    .get(/* secure this route*/, getUser)
-    .patch(/* secure this route*/, updateUser)
-    .delete(/* secure this route*/, deleteUser)
+    .get(/* secure this route*/ getUser)
+    .patch(/* secure this route*/ updateUser)
+    .delete(/* secure this route*/ deleteUser)
 
 
 module.exports = router
